94493d3c28488acd8c895cf323abbc7bc1c3a18e
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-09-23 19:50:33 -0500

    Created server.js files and updated with dependencies,express app, listener

---
+++ server.js
@@ -0,0 +1,22 @@
+// Dependencies
+// ===========================================================
+var express = require("express");
+// ===========================================================
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Listener
+// ===========================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
