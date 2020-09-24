a4ce36ed39c4e3ca2b9ccd1938cfdbf40afc1c5e
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-09-23 23:27:56 -0500

    Updated path with express.static

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 // Dependencies
 // ===========================================================
 var express = require("express");
+var path = require("path");
 // ===========================================================
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public'));
 
 // ROUTER
 require("./routes/apiRoutes")(app);
